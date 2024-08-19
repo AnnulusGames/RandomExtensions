485c72e513fcb0004269870cd72dc0eeb93f181a
AnnulusGames <annulusgames@gmail.com>
2024-08-19 22:53:31 +0900

    Fix: the definition of IWeightedCollection<T>

--- src/RandomExtensions/Collections/IWeightedCollection.cs
+++ src/RandomExtensions/Collections/IWeightedCollection.cs
@@ -2,6 +2,6 @@ namespace RandomExtensions.Collections;
 
 public interface IWeightedCollection<T> : IReadOnlyCollection<WeightedValue<T>>
 {
-    T GetItem<TRandom>(TRandom random) where TRandom : IRandom;
-    void GetItems<TRandom>(TRandom random, Span<T> destination) where TRandom : IRandom;
+    T GetItem(IRandom random);
+    void GetItems(IRandom random, Span<T> destination);
 }
